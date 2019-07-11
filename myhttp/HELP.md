36f5db31efedf73baa9ff4954a86ce4e30cd2df2
李杭杭 <lihh@cheche365.com>
2019-07-11 11:16:20 +0800

    [Summary]:添加使用netty手写http服务测试类模板和pom文件[Reviewer]:s·D·bs

---
+++ myhttp/HELP.md
@@ -0,0 +1,9 @@
+# Getting Started
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+* [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
+
+#### Description
+基于Netty编写的Http服务器，可以支持常规的接口访问，静态文件部署功能
---
+++ myhttp/src/main/java/com/lhh/myhttp/MyhttpApplication.java
@@ -0,0 +1,13 @@
+package com.lhh.myhttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyhttpApplication.class, args);
+    }
+
+}
